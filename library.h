7e56f054abe744646f003ab0e78c3bdb389374b7
@ <@>
2021-07-26 19:39:06 +0800

    build linux

--- library.h
+++ library.h
@@ -1,4 +1,12 @@
 #ifndef FRIDA_SHARED_LIBRARY_H
 #define FRIDA_SHARED_LIBRARY_H
-#include <frida-core.h>
+#ifdef _MSC_VER
+#include "devkit/windows/frida-core.h"
+#endif
+#ifdef __linux__
+#include "devkit/linux/frida-core.h"
+#endif
+#ifdef __APPLE__
+#include "devkit/macos/frida-core.h"
+#endif
 #endif //FRIDA_SHARED_LIBRARY_H
