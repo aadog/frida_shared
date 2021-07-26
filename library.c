a669e00b4703bf838a4b3340243c927cfc717750
@ <@>
2021-07-26 13:45:51 +0800

    linux supper

--- library.c
+++ library.c
@@ -1,22 +1,39 @@
-#include "library.h"
+﻿#include "library.h"
+#include "stdlib.h"
 
-int g_strlen(gchar* ptr){
+int _frida_g_strlen(gchar* ptr){
     return strlen(ptr);
 }
 
-gchar* g_error_get_message(GError *error){
+gchar* _frida_g_error_get_message(GError *error){
     return error->message;
 }
-int g_error_get_code(GError *error){
+int _frida_g_error_get_code(GError *error){
     return error->code;
 }
 
-GHashTableIter* g_hash_table_iter_new(){
+GHashTableIter* _frida_g_hash_table_iter_new(){
     size_t psize=sizeof(GHashTableIter);
     GHashTableIter* pdata=(GHashTableIter*)malloc(psize);
     memset(pdata,0,psize);
     return pdata;
 }
-void g_hash_table_iter_free(GHashTableIter* i){
+void _frida_g_hash_table_iter_free(GHashTableIter* i){
     free(i);
+}
+
+int g_strlen(gchar* ptr){
+    return _frida_g_strlen(ptr);
+}
+gchar* g_error_get_message(GError *error){
+    return _frida_g_error_get_message(error);
+}
+int g_error_get_code(GError *error){
+    return _frida_g_error_get_code(error);
+}
+GHashTableIter* g_hash_table_iter_new(){
+    return _frida_g_hash_table_iter_new();
+}
+void g_hash_table_iter_free(GHashTableIter* i){
+    _frida_g_hash_table_iter_free(i);
 }
--- library.h
+++ library.h
@@ -1,9 +1,4 @@
 #ifndef FRIDA_SHARED_LIBRARY_H
 #define FRIDA_SHARED_LIBRARY_H
 #include <frida-core.h>
-__declspec(dllexport) int g_strlen(gchar* ptr);
-__declspec(dllexport) gchar* g_error_get_message(GError *error);
-__declspec(dllexport) int g_error_get_code(GError *error);
-__declspec(dllexport) GHashTableIter* g_hash_table_iter_new();
-__declspec(dllexport) void g_hash_table_iter_free(GHashTableIter* i);
 #endif //FRIDA_SHARED_LIBRARY_H
