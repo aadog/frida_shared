1ac7b87f260f4a69aedd14f9cdd857128ef7c088
g.ph <g@google.com>
2022-05-09 11:42:01 +0800

    update

--- build_windows.bat
+++ build_windows.bat
@@ -1,8 +1,7 @@
-git pull
 python pre_windows.py
 mkdir build_windows
 cd build_windows
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
 cmake.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_DEPENDS_USE_COMPILER=FALSE -G "CodeBlocks - NMake Makefiles" ..
 cd build_windows
 nmake
